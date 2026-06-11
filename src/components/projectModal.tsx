c08be9850d233fd3d81fb0c1ad8c98953d80c80a
Lloyd Na <84310866+LloydN01@users.noreply.github.com>
2026-06-11 23:50:14 +0800

    Update projectModal.tsx

--- src/components/projectModal.tsx
+++ src/components/projectModal.tsx
@@ -52,7 +52,7 @@ const ProjectModal = ({
           <FontAwesomeIcon icon={faXmark} className="h-5 w-5" />
         </button>
 
-        <div className="rounded-xl overflow-hidden bg-stone-100 aspect-video flex items-center justify-center my-3">
+        <div className="rounded-xl overflow-hidden bg-stone-100 aspect-video flex items-center justify-center my-4">
           {asset.type === "image" ? (
             <img
               src={asset.src}
